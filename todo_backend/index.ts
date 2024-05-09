6c456d3105b5cdbfa0ddbdc095f19e221f1afebc
Rahul Dev <rahuldev7583@gmail.com>
2024-05-09 13:37:20 +0530

    todo_backend update

--- todo_backend/index.ts
+++ todo_backend/index.ts
@@ -5,7 +5,8 @@ import cors from "cors";
 require("dotenv").config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: "https://todo-by-rahul.netlify.app" }));
 
 //send to local host db
 if (process.env.DATABASE_URL) {
