e5546135754a78bed5a9525eea30cb56b8c515d2
Rahul Dev <rahuldev7583@gmail.com>
2024-05-12 02:36:56 +0530

    update

--- todo_backend/index.ts
+++ todo_backend/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 require("dotenv").config();
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 // app.use(cors({ origin: "http://localhost:5173" }));
 
 //send to local host db
