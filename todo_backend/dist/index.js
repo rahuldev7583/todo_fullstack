bc974bfc7dbf8a48e40a253363438b378e931847
Rahul Dev <rahuldev7583@gmail.com>
2024-05-12 02:24:22 +0530

    backend update

--- todo_backend/dist/index.js
+++ todo_backend/dist/index.js
@@ -8,7 +8,12 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 require("dotenv").config();
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+const allowedOrigins = ["http://localhost:5173"];
+const options = {
+    origin: allowedOrigins,
+};
+app.use((0, cors_1.default)(options));
+// app.use(cors({ origin: "http://localhost:5173" }));
 //send to local host db
 if (process.env.DATABASE_URL) {
     const mongoURI = process.env.DATABASE_URL;
@@ -27,10 +32,14 @@ app.get("/", (req, res) => {
     res.send("This is a Todo app");
 });
 app.use(express_1.default.json());
-app.use((req, res, next) => {
-    res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
-    next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Referrer-Policy", "'no-referrer'");
+//   next();
+// });
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   next();
+// });
 //Available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/task"));
--- todo_backend/index.ts
+++ todo_backend/index.ts
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 
 // app.use(cors());
-// app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "http://localhost:5173" }));
 
 //send to local host db
 if (process.env.DATABASE_URL) {
@@ -28,13 +28,28 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 // app.use((req, res, next) => {
-//   res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+//   res.setHeader("Referrer-Policy", "'no-referrer'");
 //   next();
 // });
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 
 //Available routes
 app.use("/api/auth", require("./routes/auth"));
