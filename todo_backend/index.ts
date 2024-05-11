68c4dead720f5b11c7d99932454c5d7c11e6beb2
Rahul Dev <rahuldev7583@gmail.com>
2024-05-12 02:48:40 +0530

    update backend

--- todo_backend/index.ts
+++ todo_backend/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 require("dotenv").config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 // app.use(cors({ origin: "http://localhost:5173" }));
 
 //send to local host db
@@ -31,26 +31,7 @@ app.use(express.json());
 //   res.setHeader("Referrer-Policy", "'no-referrer'");
 //   next();
 // });
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
-
+// s
 //Available routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/task"));
